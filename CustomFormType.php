3fbb820e2fc5c053464c8547f3a1e5e2f18b5b3c
Romaxx <8945610+Romaxx@users.noreply.github.com>
2019-08-05 09:31:29 +0200

    Create CustomFormType.php

---
+++ CustomFormType.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Form\Type;
+
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Contracts\Service\ServiceSubscriberInterface;
+
+class CustomFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('searchfield', SearchType::class, array(
+            'required' => true
+        ));
+        $builder->add('searchbutton',SubmitType::class,array(
+            'label'=>'Search'
+        ));
+
+        $builder->add('sub',Custom2FormType, array(
+            'label' => 'subsearch',
+        ));
+
+
+        $builder->addEventListener(
+            FormEvents::SUBMIT,
+            function (FormEvent $event) use ($options) {
+                $form = $event->getForm();
+                $data = $event->getData();
+
+                if (!is_null($form->getClickedButton())){
+                    switch ($form->getClickedButton()->getName()){
+                        case 'searchbutton':
+                           
+                            //here the problem, when searchbutton SubmitType is cliqued from sub Custom2FormType Child, this trigger occur
+                            break;
+                    }
+                }
+            }
+        );
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            //some stuff
+        ));
+
+        $resolver->setRequired(array(
+            //some stuff
+        ));
+    }
+
+}
